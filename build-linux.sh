4d8c398fe74a9cfe11553bb95d893d3dddb22ecc
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-07-28 00:14:31 -0300

    updated linux build script and add windows build script

--- build-linux.sh
+++ build-linux.sh
@@ -1,13 +1,21 @@
 #!bin/bash
 echo -e "\n\nCompiling libraries\n"
-rm ./dependencies/lib/* -f -r -d
-gcc -c ./dependencies/implementations/record.c -o ./dependencies/lib/record.o
-gcc -c ./dependencies/implementations/dir_mgmt.c -o ./dependencies/lib/dir_mgmt.o
-gcc -c ./dependencies/implementations/date_time.c -o ./dependencies/lib/date_time.o
+gcc=C:/cygwin64/bin/gcc.exe
 
-ar rv ./dependencies/lib/librecord.a ./dependencies/lib/record.o
-ar rv ./dependencies/lib/libdir_mgmt.a ./dependencies/lib/dir_mgmt.o
-ar rv ./dependencies/lib/libdate_time.a ./dependencies/lib/date_time.o
+rm ./dependencies/lib/linux/* -f -r -d
+rm ./dependencies/lib/windows/* -f -r -d
+
+gcc -c ./dependencies/implementations/error.c -o ./dependencies/lib/linux/error.o
+gcc -c ./dependencies/implementations/file.c -o ./dependencies/lib/linux/file.o
+gcc -c ./dependencies/implementations/record.c -o ./dependencies/lib/linux/record.o
+gcc -c ./dependencies/implementations/dir_mgmt.c -o ./dependencies/lib/linux/dir_mgmt.o
+gcc -c ./dependencies/implementations/date_time.c -o ./dependencies/lib/linux/date_time.o
+
+ar rv ./dependencies/lib/linux/liberror.a ./dependencies/lib/linux/error.o
+ar rv ./dependencies/lib/linux/libfile.a ./dependencies/lib/linux/file.o
+ar rv ./dependencies/lib/linux/librecord.a ./dependencies/lib/linux/record.o
+ar rv ./dependencies/lib/linux/libdir_mgmt.a ./dependencies/lib/linux/dir_mgmt.o
+ar rv ./dependencies/lib/linux/libdate_time.a ./dependencies/lib/linux/date_time.o
 
 echo -e "\nCompiling the program \n"
-gcc ./mainFile.c -o ./mainFile -L "./dependencies/lib" -I "./dependencies/include" -lrecord -ldir_mgmt -ldate_time -ansi --std=c99 -static
+gcc ./mainFile.c -o ./mainFile -L "./dependencies/lib/linux" -I "./dependencies/include" -lfile -lerror -lrecord -ldir_mgmt -ldate_time -ansi --std=c99 -static -Wall -pedantic
---
+++ build-win.bat
@@ -0,0 +1,20 @@
+echo -e "\n\nCompiling libraries\n"
+set gcc=C:\MinGW\mingw64\bin\gcc.exe
+
+del .\dependencies\lib\windows /F /S /Q
+del .\dependencies\lib\linux /F /S /Q
+
+%gcc% -c .\dependencies\implementations\error.c -o .\dependencies\lib\windows\error.o
+%gcc% -c .\dependencies\implementations\file.c -o .\dependencies\lib\windows\file.o
+%gcc% -c .\dependencies\implementations\record.c -o .\dependencies\lib\windows\record.o
+%gcc% -c .\dependencies\implementations\dir_mgmt.c -o .\dependencies\lib\windows\dir_mgmt.o
+%gcc% -c .\dependencies\implementations\date_time.c -o .\dependencies\lib\windows\date_time.o
+
+ar rv .\dependencies\lib\windows\liberror.a .\dependencies\lib\windows\error.o
+ar rv .\dependencies\lib\windows\libfile.a .\dependencies\lib\windows\file.o
+ar rv .\dependencies\lib\windows\librecord.a .\dependencies\lib\windows\record.o
+ar rv .\dependencies\lib\windows\libdir_mgmt.a .\dependencies\lib\windows\dir_mgmt.o
+ar rv .\dependencies\lib\windows\libdate_time.a .\dependencies\lib\windows\date_time.o
+
+echo -e "\nCompiling the program \n"
+%gcc% .\mainFile.c -o .\mainFile -L ".\dependencies\lib\windows" -I ".\dependencies\include" -lfile -lerror -lrecord -ldir_mgmt -ldate_time -ansi --std=c99 -static -Wall -pedantic
