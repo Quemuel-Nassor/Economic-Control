a6c352f8cd506a2dd801980c9355ec4ccd593a69
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-08-14 19:57:58 -0300

    added library in build scripts

--- build-linux.sh
+++ build-linux.sh
@@ -6,16 +6,18 @@ rm ./dependencies/lib/linux/* -f -r -d
 rm ./dependencies/lib/windows/* -f -r -d
 
 gcc -c ./dependencies/implementations/error.c -o ./dependencies/lib/linux/error.o
+gcc -c ./dependencies/implementations/string_handler.c -o ./dependencies/lib/linux/string_handler.o
 gcc -c ./dependencies/implementations/file.c -o ./dependencies/lib/linux/file.o
 gcc -c ./dependencies/implementations/record.c -o ./dependencies/lib/linux/record.o
 gcc -c ./dependencies/implementations/dir_mgmt.c -o ./dependencies/lib/linux/dir_mgmt.o
 gcc -c ./dependencies/implementations/date_time.c -o ./dependencies/lib/linux/date_time.o
 
 ar rv ./dependencies/lib/linux/liberror.a ./dependencies/lib/linux/error.o
+ar rv ./dependencies/lib/linux/libstring_handler.a ./dependencies/lib/linux/string_handler.o
 ar rv ./dependencies/lib/linux/libfile.a ./dependencies/lib/linux/file.o
 ar rv ./dependencies/lib/linux/librecord.a ./dependencies/lib/linux/record.o
 ar rv ./dependencies/lib/linux/libdir_mgmt.a ./dependencies/lib/linux/dir_mgmt.o
 ar rv ./dependencies/lib/linux/libdate_time.a ./dependencies/lib/linux/date_time.o
 
 echo -e "\nCompiling the program \n"
-gcc ./mainFile.c -o ./mainFile -L "./dependencies/lib/linux" -I "./dependencies/include" -lfile -lerror -lrecord -ldir_mgmt -ldate_time -ansi --std=c99 -static -Wall -pedantic
+gcc ./mainFile.c -o ./mainFile -L "./dependencies/lib/linux" -I "./dependencies/include" -lfile -lerror -lstring_handler -lrecord -ldir_mgmt -ldate_time -ansi --std=c99 -static -Wall -pedantic
--- build-win.bat
+++ build-win.bat
@@ -5,16 +5,18 @@ del .\dependencies\lib\windows /F /S /Q
 del .\dependencies\lib\linux /F /S /Q
 
 %gcc% -c .\dependencies\implementations\error.c -o .\dependencies\lib\windows\error.o
+%gcc% -c .\dependencies\implementations\string_handler.c -o .\dependencies\lib\windows\string_handler.o
 %gcc% -c .\dependencies\implementations\file.c -o .\dependencies\lib\windows\file.o
 %gcc% -c .\dependencies\implementations\record.c -o .\dependencies\lib\windows\record.o
 %gcc% -c .\dependencies\implementations\dir_mgmt.c -o .\dependencies\lib\windows\dir_mgmt.o
 %gcc% -c .\dependencies\implementations\date_time.c -o .\dependencies\lib\windows\date_time.o
 
 ar rv .\dependencies\lib\windows\liberror.a .\dependencies\lib\windows\error.o
+ar rv .\dependencies\lib\windows\libstring_handler.a .\dependencies\lib\windows\string_handler.o
 ar rv .\dependencies\lib\windows\libfile.a .\dependencies\lib\windows\file.o
 ar rv .\dependencies\lib\windows\librecord.a .\dependencies\lib\windows\record.o
 ar rv .\dependencies\lib\windows\libdir_mgmt.a .\dependencies\lib\windows\dir_mgmt.o
 ar rv .\dependencies\lib\windows\libdate_time.a .\dependencies\lib\windows\date_time.o
 
 echo -e "\nCompiling the program \n"
-%gcc% .\mainFile.c -o .\mainFile -L ".\dependencies\lib\windows" -I ".\dependencies\include" -lfile -lerror -lrecord -ldir_mgmt -ldate_time -ansi --std=c99 -static -Wall -pedantic
+%gcc% .\mainFile.c -o .\mainFile -L ".\dependencies\lib\windows" -I ".\dependencies\include" -lfile -lerror -lstring_handler -lrecord -ldir_mgmt -ldate_time -ansi --std=c99 -static -Wall -pedantic
