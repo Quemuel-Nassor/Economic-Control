c0f2e23d6554a072e7241f7424e395cd56d7d651
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-08-24 09:50:56 -0300

    updated test

--- test.c
+++ test.c
@@ -76,70 +76,8 @@ int main(void){
 	record *test = new_record_overloaded(1,26.74,new_datetime(),"conteudo aleatorio para serializacao de dados",5,"ab yuu27 129921 fdkjfdkjfkdjfkdfj");
 	test->add(test,new_record_overloaded(2,105.98,new_datetime(),"alguma coisa para preencher",15,"eita lasqeura"));
 	test->add(test,new_record_overloaded(3,49.67,new_datetime(),"ola teste ops",67,"lorem ipsum"));
-	test->add(test,new_record_overloaded(4,26.74,new_datetime(),"conteudo aleatorio para serializacao de dados",12,"ab yuu27 129921 fdkjfdkjfkdjfkdfj"));
+	test->add(test,new_record_overloaded(4,15.24,new_datetime(),"conteudo aleatorio para serializacao de dados",12,"ab yuu27 129921 fdkjfdkjfkdjfkdfj"));
 	
-	
-	// record *percursor = test;
-	// for (percursor = test; percursor != NULL; percursor = percursor->next)
-	// {
-	// 	percursor->show(percursor);
-	// }
-	
-		// printf("%s",string_join(INDEFINITE_LENGTH,EMPTY_SEPARATOR,
-        // ( test->prev == NULL ? "[{" : ",{"),
-        // "\"Id\":","id",
-        // ",\"Description\":\"",test->description,
-        // "\",\"Category_id\":","category",
-        // ",\"Details\":\"",test->details,
-        // "\",\"Datetime\":\"",test->datetime.format_string(test->datetime,DATABASE_UTC),
-        // "\",\"Value\":","value",
-        // (test->next == NULL ? "}]" : "}"),NULL));
-
-	// // ------------------ área para teste
-	// char *json_result = (char*)malloc(sizeof(char)*2);
-    // strcpy(json_result,"\0");
-    // record *element = navigate_to_start(test);
-        
-    // for(element; element != NULL; element = element->next)
-    // {
-    //     int size_id = snprintf( NULL, 0, "%li", element->id ) + 1;
-    //     int size_category = snprintf( NULL, 0, "%li", element->category_id ) + 1;
-    //     int size_value = snprintf( NULL, 0, "%Lf", element->value ) + 1;
-    //     size_t size = DATETIME_MAX_LENGTH + MAX_DESCRIPTION + MAX_DETAILS + size_id + size_category + size_value + strlen(json_result) + 77;
-        
-    //     char *id = (char*)malloc(sizeof(char)*size_id);
-    //     char *category = (char*)malloc(sizeof(char)*size_category);
-    //     char *value = (char*)malloc(sizeof(char)*size_value);
-
-    //     sprintf(id, "%li", element->id);
-    //     id[strlen(id)] = '\0';
-    //     sprintf(category, "%li", element->category_id);
-    //     category[strlen(category)] = '\0';
-    //     sprintf(value, "%Lf", element->value);
-    //     value[strlen(value)] = '\0';
-        
-    //     char* aux = json_result;
-    //     json_result = (char*)malloc(sizeof(char)*size);
-    //     strcpy(json_result,aux);
-
-    //     strcat(json_result,string_join(INDEFINITE_LENGTH,EMPTY_SEPARATOR,
-    //     ( element->prev == NULL ? "[{" : ",{"),
-    //     "\"Id\":",id,
-    //     ",\"Description\":\"",element->description,
-    //     "\",\"Category_id\":",category,
-    //     ",\"Details\":\"",element->details,
-    //     "\",\"Datetime\":\"",element->datetime.format_string(element->datetime,DATABASE_UTC),
-    //     "\",\"Value\":",value,
-    //     (element->next == NULL ? "}]" : "}"),NULL));
-    
-    //     free(id);
-    //     free(aux);
-    //     free(value);
-    //     free(category);
-
-    // }
-	// // ------------------ área para teste
-
 	char *json_result = serialize_to_json(test);
 	printf("json: \n%s",json_result);
 	
